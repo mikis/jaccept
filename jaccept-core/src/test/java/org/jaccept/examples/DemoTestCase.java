447de290309e0f9c9fd06107d44c85b78fd66ace
Mikis Seth Sørensen <mail@mikis.dk>
2006-04-17 19:45:48 +0000

    Moved example to test
    git-svn-id: https://jaccept.svn.sourceforge.net/svnroot/jaccept/trunk@14 16cdb8af-570e-0410-a9df-a1c2ad36d4c5

---
+++ jaccept-core/src/test/java/org/jaccept/examples/DemoTestCase.java
@@ -0,0 +1,76 @@
+package org.jaccept.examples;
+
+import junit.framework.Test;
+
+import org.jaccept.CaseLevelSuite;
+import org.jaccept.ExtendedTestCase;
+import org.jaccept.ExtendedTestSuite;
+import org.jaccept.TestEventManager;
+
+public class DemoTestCase extends ExtendedTestCase {
+    ExampleComponentWrapper c2t = new ExampleComponentWrapper(new ExampleComponent());
+	
+    public DemoTestCase(String string, String description) {
+        super(string, description);
+    }
+    
+    public DemoTestCase(String string) {
+        super(string);
+    }
+    
+    public static Test suite() {
+        ExtendedTestSuite suite= new CaseLevelSuite("DemoCase", DemoTestCase.class);
+        suite.addDescription("Case level description case ");
+        suite.addReference("Requirement 1.12.2");
+        suite.addTest(new DemoTestCase("firstMethodTest", "Goodcasetest of the firstMethod"));
+        suite.addTest(new DemoTestCase("firstMethodNullTest", "Null parameter test of the firstMethod"));
+        return suite;
+    }
+
+	public void firstMethodTest() {
+        addDescription("Test level description test 1");
+        
+        addStep("Create a component2Test instance", "");
+        c2t = new ExampleComponentWrapper(new ExampleComponent());
+    	
+    	addStep("Call the setListener method with a testlistener", 
+    	"Testlistener should be set on component2Test");
+    	c2t.setListener(new ComponentLister() {
+    		public void firstMethodCalled(String param) {
+    			TestEventManager.getInstance().addResult("TestListener called with param "+param);
+    		}
+    	});
+        addStep("Call the c2t with the string 'alpha'", 
+        		"The testlistener should be notified of the call and the string 'returnValue' should be returned");
+        assertEquals("Wrong return value", "returnValue", c2t.firstMethod("alpha"));
+        
+        addStep("Call the c2t with the string 'beta'", 
+		"The testlistener should be notified of the call again and the string 'returnValue' should be still be returned");
+        assertEquals("Wrong return value", "returnValue", c2t.firstMethod("beta"));
+    }    
+
+    public void firstMethodNullTest() { 
+        addDescription("Verify that the component2Test handles null parameters correctly");
+        addReference("-------------");
+        
+        addStep("Create a component2Test instance", "");
+        c2t = new ExampleComponentWrapper(new ExampleComponent());
+    	
+    	addStep("Call the setListener method with a testlistener", 
+    	"Testlistener should be set on component2Test");
+    	c2t.setListener(new ComponentLister() {
+    		public void firstMethodCalled(String param) {
+    			TestEventManager.getInstance().addResult("TestListener called with param "+param);
+    		}
+    	});
+    	
+        addStep("Call the c2t with the null", 
+        		"The testlistener should be notified of the call and the string 'returnValue' should be returned");
+        assertEquals("Wrong return value", "returnValue", c2t.firstMethod(null));
+        
+    }
+    
+    public void testDummy() {
+    	
+    }
+}
---
+++ jaccept-core/src/test/java/org/jaccept/examples/ExampleComponent.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.jaccept.examples;
+
+
+class ExampleComponent {
+//	 private final Logger log = Logger.getLogger(Component2Test.class.getName());
+	 private ComponentLister listener;
+	 
+	 /**
+	  * Called th componentlistener with the supplied value
+	  * @param param The value to give to the listener
+	  * @return The string 'returnValue'
+	  */
+	 public String firstMethod(String param) {
+//		 log.info("firstMethod called with parameters"+param);
+		 listener.firstMethodCalled(param);
+		 return "returnValue";
+	 }
+	 
+	 public void setListener(ComponentLister listener) {
+		 this.listener = listener;
+	 }
+}
+
+interface ComponentLister {
+	public void firstMethodCalled(String param);
+}
